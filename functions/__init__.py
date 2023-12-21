86d53ec051879000ed060fe862172ce8023fdd07
Min Seok Kim <minseok.kim@brain.snu.ac.kr>
2023-12-21 10:53:31 +0900

    :sparkles: Update new functions.

---
+++ functions/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+
+# importer.py
+from .importer import import_data
+from .importer import make_toydata
+
+# plots.py
+from .plots import make_xticks
+from .plots import make_xlabels
+from .plots import make_ylabels
+from .plots import heatmap
+from .plots import save_figure
